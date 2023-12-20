0fc0b612e399f2a5e9e0b15930b47b91ceee2cff
Diego Tadeu Martins Acioly Ribeiro Dias <Diego.acioly@hotmail.com>
2023-12-20 19:23:19 -0300

    -clean

---
+++ design-patterns-cs-master/PatternsCriacao/AbstractFactory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = ExecutaAbstractFactory.montarCarro("luxo");
+            Carro c2 = ExecutaAbstractFactory.montarCarro("popular");
+        }
+    }
+}
---
+++ design-patterns-cs-master/PatternsCriacao/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public class FactoryMethod
+    {
+        public IPersonagem Escolher_Personagem(string personagem)
+        {
+            switch (personagem)
+            {
+                case "Liu Kang": return new LiuKang();
+                case "SubZero": return new SubZero();
+                case "Scorpion": return new Scorpion();
+                default: return null;
+            }
+        }
+    }
+}
